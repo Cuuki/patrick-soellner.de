7df81cf4892101afbee894d073fdd04fbbc7d60c
Patrick Söllner <mail@patrick-soellner.de>
2022-04-06 22:44:01 +0200

    chore: optimise structure and packages

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require('next-transpile-modules')(['ui']);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withTM({
