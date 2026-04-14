f11a645279eee86f5be16605c840decece5a6051
Patrick Söllner <mail@patrick-soellner.de>
2026-04-14 23:42:17 +0200

    fix deprecated symbol

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import 'sanitize.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'theme-ui';
+import { ThemeUIProvider } from 'theme-ui';
 import theme from '../utils/theme';
+
+import 'sanitize.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeUIProvider theme={theme}>
       <Component {...pageProps} />
-    </ThemeProvider>
+    </ThemeUIProvider>
   );
 }
